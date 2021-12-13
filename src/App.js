9028f13a02833eec8ed0f3db652659e64019ca20
Emilie Anthony <emilieanthony@live.se>
2021-12-13 21:32:47 +0100

    Add wrapper component Card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -6,26 +7,8 @@ function App() {
     { title: "Car Insurance", amount: 294.56, date: new Date(2021, 2, 28) },
     { title: "Insurance", amount: 100.65, date: new Date(2021, 9, 28) },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-    </div>
-  );
+
+  return <Expenses items={expenses} />;
 }
 
 export default App;
