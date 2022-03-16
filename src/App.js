b746816f7ea9a892524f60c2d15050c2eb0f3670
Emilie Anthony <emilieanthony@live.se>
2022-03-16 15:22:00 +0100

    add rendering list of expenses

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const INITIAL_EXPENSES = [
+  { title: "Car Insurance", amount: 294.56, date: new Date(2021, 2, 28) },
+  { title: "Insurance", amount: 100.65, date: new Date(2021, 9, 28) },
+];
+
 const App = () => {
-  const expenses = [
-    { title: "Car Insurance", amount: 294.56, date: new Date(2021, 2, 28) },
-    { title: "Car Insurance", amount: 294.56, date: new Date(2021, 2, 28) },
-    { title: "Insurance", amount: 100.65, date: new Date(2021, 9, 28) },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
