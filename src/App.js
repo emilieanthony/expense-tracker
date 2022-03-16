c4299ca5bca414ecf74dfe3fe0f7b55f1eb6cacc
Emilie Anthony <emilieanthony@live.se>
2022-03-16 15:38:03 +0100

    add id key as prop for expense item in order to reduce number of iterations over expense list

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const INITIAL_EXPENSES = [
-  { title: "Car Insurance", amount: 294.56, date: new Date(2021, 2, 28) },
-  { title: "Insurance", amount: 100.65, date: new Date(2021, 9, 28) },
+  { id: 'e1', title: "Car Insurance", amount: 294.56, date: new Date(2021, 2, 28) },
+  { id: 'e2', title: "Insurance", amount: 100.65, date: new Date(2021, 9, 28) },
 ];
 
 const App = () => {
